cb8e98e6bd2dda166ea52707902650912f3e5c4c
eurekasolution <nadopro@gmail.com>
2022-05-19 17:29:20 +0900

    2022-05-19 Fin

--- src/main/kotlin/com/kbstar/kotlin/A06Operator.kt
+++ src/main/kotlin/com/kbstar/kotlin/A06Operator.kt
@@ -31,7 +31,32 @@ fun main() {
         비트연산자
             4.shl(2), 7.shr(2), 9.and(3)
 
-
-
      */
+
+    var x = 4   // 100
+    var y = 0b0000_1010 // 10
+    var z = 0x0F    // 0000 1111
+
+    println("x shl 2 = ${x shl 2}")
+    println("x.inv() = ${x.inv()}")
+    // 0000 0000   0000 0000   0000 0000   0000 0100
+    // 1111 1111   1111 1111   1111 1111   1111 1011
+
+    println("y shr 2 = ${y shr 2}   or ${y/4}")
+    println("y shl 4 = ${y shl 4}  or ${ y * 16}")
+
+    var k = x.shl(3)
+
+    num1 = 12
+    num2 = 25
+    var result :Int
+
+    result = num1 or num2  // num1 | num2
+    println("result = ${result}")
+
+    // 01100
+    // 11001
+    // 11101 : 29
+
+
 }
